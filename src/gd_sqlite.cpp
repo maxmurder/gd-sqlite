314217d0cc5f7dd93a99d9b5789244df3532772b
maxmurder <rusty657@shaw.ca>
2017-09-29 22:16:03 -0700

    start work on table creation method

--- src/gd_sqlite.cpp
+++ src/gd_sqlite.cpp
@@ -2,9 +2,10 @@
 #include "sqlite/sqlite3.h"
 #include <OS.hpp>
 
-int GDSqlite::open(Variant filename, bool bInMemory)
+int GDSqlite::open(Variant filename, bool _bInMemory)
 {
-    String path = bInMemory ? String("file::memory:") : OS::get_data_dir() + String("/") + filename;
+    bInMemory = _bInMemory;
+    path = _bInMemory ? String("file::memory:") : OS::get_data_dir() + String("/") + filename;
     int ret = sqlite3_open(path.c_string(), &db);
     if (ret != SQLITE_OK)
     {
@@ -18,8 +19,30 @@ void GDSqlite::close()
     sqlite3_close(db);
 }
 
+String GDSqlite::create_table(Variant _name, Variant _columns)
+{
+    String name = _name;
+    Array columns = _columns;
+    char query[128];
+    sprintf(query, "CREATE TABLE IF NOT EXISTS `%s` (", name.c_string());
+    for(int itr=0; itr<columns.size(); itr++)
+    {
+        String column = columns[itr];
+        sprintf(query, "%s%s%s", query, (itr != 0 ? "," : ""), column.c_string());
+    }
+    sprintf(query, "%s);", query);
+    return query;
+}
+
+String GDSqlite::get_path()
+{
+    return path;
+}
+
 void GDSqlite::_register_methods() 
 {
-    register_method((char *)"Open", &GDSqlite::open);
-    register_method((char *)"Close", &GDSqlite::close);
+    register_method((char *)"open", &GDSqlite::open);
+    register_method((char *)"close", &GDSqlite::close);
+    register_method((char *)"create_table", &GDSqlite::create_table);
+    register_method((char *)"get_path", &GDSqlite::get_path);
 }
--- src/gd_sqlite.h
+++ src/gd_sqlite.h
@@ -9,12 +9,21 @@ using namespace godot;
 class GDSqlite : public GodotScript<Reference> { 
     GODOT_CLASS(GDSqlite)
 public:
-    static void _register_methods();
-    
     int open(Variant filename, bool bInMemory = false);
     void close();
+    String create_table(Variant name, Variant columns);
+    
+    String get_path();
 private: 
     class sqlite3 *db;
+    bool bInMemory;
+    String path;
+public:
+    static void _register_methods();
+    ~GDSqlite()
+    {
+        close();
+    }
 };
 
 #endif
