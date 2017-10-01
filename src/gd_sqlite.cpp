4baff1663512bd052ee31f4717b3952896ca9ede
maxmurder <rusty657@shaw.ca>
2017-10-01 14:15:53 -0700

    got advanced query working, added licence message

--- src/gd_sqlite.cpp
+++ src/gd_sqlite.cpp
@@ -1,3 +1,25 @@
+/* MIT License
+ *
+ * Copyright (c) 2017 maxmurder
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include "gd_sqlite.h"
 #include "sqlite/sqlite3.h"
 #include <OS.hpp>
@@ -14,16 +36,17 @@ int GDSqlite::open(Variant filename, bool _bInMemory)
     return ret;
 }
 
-void GDSqlite::close()
+int GDSqlite::close()
 {
-    sqlite3_close(db);
+   return sqlite3_close(db);
 }
 
 int GDSqlite::create_table(Variant _name, Variant _columns)
 {
     String name = _name;
     Array columns = _columns;
-    // Generate query
+    int ret = -1;
+    // generate query
     char query[128];
     sprintf(query, "CREATE TABLE IF NOT EXISTS `%s` (", name.c_string());
     for(int itr=0; itr<columns.size(); itr++)
@@ -33,8 +56,7 @@ int GDSqlite::create_table(Variant _name, Variant _columns)
     }
     sprintf(query, "%s);", query);
     // compile & execute statement
-    int ret = -1;
-    if(prepare_stmt(query) == SQLITE_OK)
+    if(prepare(query) == SQLITE_OK)
     {
         ret = step();
         finalize();
@@ -46,7 +68,7 @@ int GDSqlite::query(Variant _query)
 { 
     int ret = -1;
     String query = _query;
-    if( prepare_stmt(query.c_string()) == SQLITE_OK )
+    if( prepare(query.c_string()) == SQLITE_OK )
     {
         ret = step();
         finalize();
@@ -54,12 +76,48 @@ int GDSqlite::query(Variant _query)
     return ret;
 }
 
+Array GDSqlite::query_array( Variant _query )
+{
+    Array out;
+    String query = _query;
+    // construct output array from the query 
+    if( prepare(query.c_string()) == SQLITE_OK)
+    {
+        int ret = step();
+        while (ret == SQLITE_ROW) {
+            Dictionary row;
+            // add all columns to each row
+            for(int col = 0; col < sqlite3_column_count(stmt); col++)
+            {
+               // handle diffrent column types
+               switch(sqlite3_column_type(stmt, col))
+               {
+                    case SQLITE_INTEGER: 
+                        row[sqlite3_column_name(stmt, col)] = sqlite3_column_int(stmt, col);
+                        break;
+                    case SQLITE_FLOAT:
+                        row[sqlite3_column_name(stmt, col)] = sqlite3_column_double(stmt, col);
+                        break;
+                    case SQLITE_TEXT:
+                        row[sqlite3_column_name(stmt, col)] = String((char *)sqlite3_column_text(stmt, col));
+                        break;
+               }
+            }
+            // store result and iterate
+            out.push_back(row);
+            ret = step();
+        }
+    }
+    finalize();
+    return out;
+}
+
 String GDSqlite::get_path()
 {
     return path;
 }
 
-int GDSqlite::prepare_stmt(const char* query)
+int GDSqlite::prepare(const char* query)
 {
     return sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
 }
@@ -69,8 +127,8 @@ int GDSqlite::step()
     return sqlite3_step(stmt);
 }
 
-void GDSqlite::finalize()
+int GDSqlite::finalize()
 {
-    sqlite3_finalize(stmt);
+    return sqlite3_finalize(stmt);
 }
 
--- src/gd_sqlite.h
+++ src/gd_sqlite.h
@@ -1,3 +1,25 @@
+/* MIT License
+ *
+ * Copyright (c) 2017 maxmurder
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef GDSQLITE_H
 #define GDSQLITE_H
 
@@ -9,21 +31,60 @@ using namespace godot;
 class GDSqlite : public GodotScript<Reference> { 
     GODOT_CLASS(GDSqlite)
 public:
+    /* Opens a database file for read/write.
+     * @param filename  Variant<String> 
+     *                  Name of slqite database file to be opened. Files are 
+     *                  saved to the godot app_userdata folder.
+     * @param bInMemory bool
+     *                  If true database is opened as an slqite3 in-memory
+     *                  database.
+     * @return int      sqlite3 return code.
+     */                
     int open(Variant filename, bool bInMemory = false);
-    void close();
+    
+    /* Closes the currently open database. */
+    int close();
+    
+    /* Creates a new table in the database.
+     * @param name      Variant<String>
+     *                  Name of table to be created.
+     * @param columns   Variant<Array<String>>
+     *                  List of columns to be added to created table. Follows
+     *                  traditional SQL syntax.
+     *                  ["id INTEGER PRIMARY KEY AUTOINCREMENT", "name TEXT"]
+     * @return int      sqlite3 return code.
+     */
     int create_table(Variant name, Variant columns);
+    
+    /* Simple query, best used for insertions etc.
+     * @param query     Variant<String>
+     *                  SQL query to be executed on database. Follows 
+     *                  traditional SQL syntax.
+     * @return int      sqlite3 return code.
+     */
     int query(Variant query);
     
+    /* Query which returns an array of the query results, best used for
+     * selections etc.
+     * @param query     Variant<String>
+     *                  SQL query to be executed on the database Follows
+     *                  Traditional SQL syntax.
+     * @return Array<Dictionary> Array of rows returned by the query.
+     */
+    Array query_array(Variant query);
+    
+    /* Gets the path to the sqlite3 database file. */
     String get_path();
+    
 private: 
     class sqlite3 *db;
     class sqlite3_stmt *stmt;
     bool bInMemory;
     String path;
     
-    int prepare_stmt(const char* query);
+    int prepare(const char* query);
     int step();
-    void finalize();
+    int finalize();
 public:
     static void _register_methods()
     {
@@ -31,6 +92,7 @@ public:
         register_method((char *)"close", &GDSqlite::close);
         register_method((char *)"create_table", &GDSqlite::create_table);
         register_method((char *)"query", &GDSqlite::query);
+        register_method((char *)"query_array", &GDSqlite::query_array);
         register_method((char *)"get_path", &GDSqlite::get_path);
     }
     ~GDSqlite()
